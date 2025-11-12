4f43abc600e83d994b8ed0abf3395af4ba41a539
AbdulrahmanAhmedZaghloul <abdozaghloul990@gmail.com>
2025-11-12 22:59:47 +0200

    wrap App component with React StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
